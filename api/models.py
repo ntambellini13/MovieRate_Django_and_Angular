72c9b0cf6111adeae5d18c280afe618c71aab073
ntambellini13 <ntambellini13@gmail.com>
2021-05-20 08:51:38 -0600

    Add model properties for number_of_ratings and average_rating for a movie

--- api/models.py
+++ api/models.py
@@ -7,13 +7,28 @@ class Movie(models.Model):
     title = models.CharField(max_length=32)
     description = models.TextField(max_length=360)
 
+    @property
+    def number_of_ratings(self):
+        ratings = Rating.objects.filter(movie=self)
+        return len(ratings)
+
+    @property
+    def average_rating(self):
+        sum = 0
+        ratings = Rating.objects.filter(movie=self)
+        for rating in ratings:
+            sum += rating.stars
+        if len(ratings) > 0:
+            return sum / len(ratings)
+        else:
+            return None
+
 
 class Rating(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     stars = models.IntegerField(validators=[MinValueValidator(1),
                                             MaxValueValidator(5)])
-
     class Meta:
         unique_together = (('user', 'movie'),)
         index_together = (('user', 'movie'),)
