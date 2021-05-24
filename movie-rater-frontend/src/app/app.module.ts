be85717b491e5a853f85f255b0206d0a3836ca12
ntambellini13 <ntambellini13@gmail.com>
2021-05-24 11:49:42 -0600

    Add angular frontend base and routing

---
+++ movie-rater-frontend/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'
+
+
+import { AuthModule } from './auth/auth.module';
+import { MainModule } from './main/main.module';
+
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'movies'}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AuthModule,
+    MainModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
